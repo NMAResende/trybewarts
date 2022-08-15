5532c0dd019bdfa5ab78d4c4df7b81a2e45ea030
Giulliana Rangel <seuemail@exemplo.br>
2022-08-15 15:38:38 -0300

    finalização do exercicio 21

--- script.js
+++ script.js
@@ -46,6 +46,63 @@ function count(event) {
 
 textArea.addEventListener('keyup', count);
 
-function informations() {
-  let userName = localStorage.getItem('#input-name');
+const formData = document.querySelector('#evaluation-form');
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+const materia = document.querySelectorAll('.subject');
+const main = document.querySelector('main');
+
+const familia = document.getElementsByName('family');
+function selectingFamily() {
+  let result = '';
+  for (let i = 0; i < familia.length; i += 1) {
+    if (familia[i].checked) {
+      result = familia[i].value;
+    }
+  }
+  return result;
 }
+
+function selectingMaterias() {
+  let result = '';
+  for (let i = 0; i < materia.length; i += 1) {
+    if (materia[i].checked) {
+      result += `${materia[i].value}, `;
+    }
+  }
+  return result;
+}
+
+function evaluation() {
+  const avaliacao = document.getElementsByName('rate');
+  let result = '';
+  for (let i = 0; i < avaliacao.length; i += 1) {
+    if (avaliacao[i].checked) {
+      result = avaliacao[i].value;
+    }
+  }
+  return result;
+}
+
+function selectedHouse() {
+  return house.value;
+}
+
+btnSubmit.addEventListener('click', () => {
+  const newForm = document.createElement('div');
+  newForm.setAttribute('id', 'evaluation-form');
+  const p = document.createElement('p');
+  p.setAttribute('id', 'form-data');
+  p.innerText = `Nome: ${nome.value} ${sobrenome.value}
+Email: ${email.value}
+Casa: ${selectedHouse()}
+Família: ${selectingFamily()}
+Matérias: ${selectingMaterias()}
+Avaliação: ${evaluation()}
+Observações: ${textArea.value}`;
+  main.removeChild(formData);
+  main.appendChild(p);
+  main.appendChild(newForm);
+  newForm.style.display = 'none';
+});
